9a6126998ae89e435fc3758a117197e5a8faa1dd
Miguel <m.nunezpeiro@gmail.com>
2024-08-28 09:45:24 +0200

    Included function to remove commas from path to data. Linting adjustments.

--- config_tool.py
+++ config_tool.py
@@ -1,22 +1,19 @@
+''' Module to run the main code of the tool.'''
+
 # -*- coding: utf-8 -*-
-"""
-Created on 2024-08-01
 
-@author: Jesus Lizana
-
-"""
-
-                      
-#%%	
-###########################################################
-
-#INPUT DATA required to run the main code
-
-###########################################################
+#%%
+#Function to read the path to the #Data folder
+def read_first_line(file_path):
+    '''
+    Reads the first line of a file and returns it as a string, removing any leading/trailing quotes.
+    '''
+    with open(file_path, "r", encoding="utf-8") as f:
+        first_line = f.readline().strip()
+    # Remove leading/trailing quotes if present
+    cleaned_line = first_line.strip('\'"')
+    return cleaned_line
 
 #Data folder
 #read first line of *.txt file with name path_to_data
-with open("path_to_data.txt", "r") as f:
-    cwd_data = f.readline().strip()
-
-
+cwd_data = read_first_line('path_to_data.txt')
